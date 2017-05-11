ecd5252024339c0dbc2aa8e175e9c43f825fb6cd
mxy940127 <634666730@qq.com>
2017-05-11 17:39:12 +0800

    89ms need to improve

--- LeetCode/src/cn/jygenius/Solution.java
+++ LeetCode/src/cn/jygenius/Solution.java
@@ -83,4 +83,21 @@ public class Solution {
 	            num=num^nums[x];
 	            return num;
 	}
+	public int[] constructRectangle(int area) {
+		int val = (int) Math.sqrt(area);
+		int[] result = new int[2];
+		for (int i = val; i <= area; i++) {
+			if (area % i == 0) {
+				if (i > area / i) {
+					result[0] = i;
+					result[1] = area / i;
+				} else {
+					result[0] = area / i;
+					result[1] = i;
+				}
+				break;
+			}
+		}
+		return result;
+	}
 }
